45d67df9c56c97502ab276b1576605acbf0d5909
weein_yg <1334986339@qq.com>
2020-09-23 14:15:12 +0800

    整理文件

--- src/main.js
+++ src/main.js
@@ -7,14 +7,11 @@ import store from './store'
 import './config/rem'
 import { getHttp, postHttp,uploadImg} from './http/index'
 import _ from 'lodash'
-import VueCookies from 'vue-cookies'
-Vue.use(VueCookies)
-//样式
-import './assets/css/normalize.css'
-//阿里
-// import './assets/iconfont/iconfont.css'
 Vue.config.productionTip = false;
-// 引入字体文件
+// import VueCookies from 'vue-cookies'
+// Vue.use(VueCookies)
+
+// 引入阿里字体样式文件(自己下载即可)
 import '../src/styles/index.css'
 
 Vue.prototype.$get = getHttp;
@@ -22,9 +19,6 @@ Vue.prototype.$post = postHttp;
 Vue.prototype.$postImg = uploadImg;
 Vue.prototype.$lodash=_;
 Vue.use(Vant);
-let options = {
-  fullscreenEl: false
-};
 new Vue({
   router,
   store,
