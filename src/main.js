679986c58e4cc9eccbea546099174da5cd8083c6
weein_yg <1334986339@qq.com>
2020-09-23 14:26:58 +0800

    修改文件

--- src/main.js
+++ src/main.js
@@ -1,24 +1,28 @@
 import Vue from 'vue'
-import Vant from 'vant';
-import 'vant/lib/index.css';
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './config/rem'
-import { getHttp, postHttp,uploadImg} from './http/index'
+//引入适配文件
+import './remconfig/rem'
+//引入请求文件
+import { getHttp, postHttp,uploadImg} from './request/index'
 import _ from 'lodash'
 Vue.config.productionTip = false;
-// import VueCookies from 'vue-cookies'
-// Vue.use(VueCookies)
 
-// 引入阿里字体样式文件(自己下载即可)
+import VueCookies from 'vue-cookies'
+Vue.use(VueCookies)
+
+//引入vant
+import Vant from 'vant';
+import 'vant/lib/index.css';
+Vue.use(Vant);
+// 引入阿里字体样式文件
 import '../src/styles/index.css'
 
 Vue.prototype.$get = getHttp;
 Vue.prototype.$post = postHttp;
 Vue.prototype.$postImg = uploadImg;
 Vue.prototype.$lodash=_;
-Vue.use(Vant);
 new Vue({
   router,
   store,
